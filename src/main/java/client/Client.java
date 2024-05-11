52a74f4d5315ebf8d448b3d0a7388824a87170cc
TwillowT <tina.losin@cqumail.com>
2024-05-12 09:52:39 +1000

    Updated file descriptions

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,6 +1,7 @@
 /*
     Students: Tina Losin (10569238)
-    Description: 
+    Description: Manages client-side operations, including user interface interaction for customer registration, product selection, 
+                 placing orders, and administrative tasks such as managing products, delivery schedules, and user accounts.
     
     
  */
