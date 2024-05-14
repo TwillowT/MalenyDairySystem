e1d9c49957277bc9fd05cf37e24ec0c35335938c
TwillowT <tina.losin@cqumail.com>
2024-05-14 18:10:35 +1000

    Updated login and registration views/controllers

--- src/main/java/com/malenydairysystem/controller/InitialViewController.java
+++ src/main/java/com/malenydairysystem/controller/InitialViewController.java
@@ -4,13 +4,26 @@
  */
 package com.malenydairysystem.controller;
 
+import java.io.IOException;
+import java.net.URL;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class InitialViewController
 {
+    @FXML
+    private TextField emailInput;
+    
+    @FXML
+    private TextField passwordInput;
+     
 
     @FXML
     private void clickExit()
@@ -24,4 +37,37 @@ public class InitialViewController
             }
         });
     }
+    
+       
+     @FXML
+    private void handleSignin(){
+        
+    } 
+    
+    // not functioning correctly - I will fix this tomorrow
+    @FXML
+    private void handleRegisterLink() throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("RegistrationView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
+    
+    
+    @FXML
+    private void handleEmailInput(){
+        
+    }
+    
+    @FXML
+    private void handlePasswordInput(){
+        
+    }
+    
+    
 }
--- src/main/java/com/malenydairysystem/controller/RegistrationViewController.java
+++ src/main/java/com/malenydairysystem/controller/RegistrationViewController.java
@@ -5,6 +5,87 @@
 
 package com.malenydairysystem.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
 public class RegistrationViewController {
     
+    @FXML
+    private TextField nameInput;
+     
+    @FXML
+    private TextField addressInput;
+            
+    @FXML
+    private TextField phoneInput;
+    
+    @FXML
+    private TextField emailInput;
+    
+    @FXML
+    private TextField passwordInput;
+            
+    @FXML
+    private TextField reEnteredPasswordInput;
+    
+    
+    
+    @FXML
+    private void clickExit()
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to Exit?");
+        alert.showAndWait().ifPresent(response ->
+        {
+            if (response == ButtonType.OK)
+            {
+                Platform.exit();
+            }
+        });
+    }
+    
+   @FXML
+    private void handleRegistration(ActionEvent event){
+        
+    }
+    
+    @FXML
+    private void handleSigninLink(ActionEvent event){
+        
+    }
+          
+    @FXML
+    private void handleNameInput(){
+        
+    }
+    
+    @FXML
+    private void handleAddressInput(){
+        
+    }
+    
+    @FXML
+    private void handlePhoneInput(){
+        
+    }
+    
+    @FXML
+    private void handleEmailInput(){
+        
+    }
+    
+    @FXML
+    private void handlePasswordInput(){
+        
+    }
+    
+    @FXML
+    private void handleReEnteredPasswordInput(){
+        
+    }
+    
+ 
 }
